44e33b13081611161930274fbd73263ec3c2d2cd
Mario Gleichmann <mario.gleichmann@orcas.de>
2022-10-21 15:10:19 +0200

    Session 4
    - Highscore will be saved to database.
      - Install (under Windows you can use XAMPP)
        - Apache Webserver
        - MariaDB Database
        - PHP
      - Prepare database.
        - Create database `bubbles`
        - Create database table `highscore` (@see /database/create.sql)
      - Configure Apache Webserver VHost.
        - hostname `bubbles` in `/etc/hosts` (Linux) or `C:\Windows\System32\drivers\etc\hosts`
        - vhost to redirect request to `http://bubbles` to project dir
    - New bubbles.php to handle reading and writing from/to database.
    - New Points counter.

--- js/bubble.js
+++ js/bubble.js
@@ -1,6 +1,8 @@
 let lastId = 0;
-let speed = 0.2;
+let speed = 1;
 let time = 10;
+let hits = 0;
+let points = 0;
 let createInterval = null;
 let moveInterval = null;
 let speedInterval = null;
@@ -13,13 +15,85 @@ function init() {
     legendInterval = setInterval(function() { legendUpdate(); }, 1000);
 }
 
+function highscore(data) {
+    let rows = JSON.parse(data);
+    let table = "" +
+        "  <table id=\"highscoreTable\">\n" +
+        "    <thead>\n" +
+        "      <tr>\n" +
+        "        <th>Platz</th>\n" +
+        "        <th>Punkte</th>\n" +
+        "        <th>Vorname</th>\n" +
+        "        <th>Nachname</th>\n" +
+        "        <th></th>\n" +
+        "      </tr>\n" +
+        "    </thead>\n" +
+        "    <tbody>\n";
+    let foundedRow = -1;
+    let pos = 1;
+    for (let r=0; r <= rows.length; r++) {
+        let row = r === rows.length && foundedRow < 0 ? { 'points': 0 } : rows[r];
+        if (foundedRow < 0 && row['points'] <= points) {
+            foundedRow = r;
+            if (row['points'] < points) {
+                table += "" +
+                    "      <tr class='input'>\n" +
+                    "        <td>" + pos + "</td>\n" +
+                    "        <td>" + points + "</td>\n" +
+                    "        <td><input type='text' id='firstname' name='firstname'></td>\n" +
+                    "        <td><input type='text' id='lastname' name='lastname'></td>\n" +
+                    "        <td><input type='button' value='Speichern' onclick='save()'></td>\n" +
+                    "      </tr>\n";
+            }
+        }
+        if(r < rows.length) {
+            table += "" +
+                "      <tr" + (foundedRow === r ? " class='current'" : "") + ">\n" +
+                "        <td>" + pos + "</td>\n" +
+                "        <td>" + row['points'] + "</td>\n" +
+                "        <td>" + row['firstname'] + "</td>\n" +
+                "        <td>" + row['lastname'] + "</td>\n" +
+                "        <td></td>\n" +
+                "      </tr>\n";
+        }
+        if (pos > 10) {
+            break;
+        }
+        pos++;
+    }
+    table += "" +
+        "    </tbody>\n" +
+        "  </table>";
+    document.getElementById("highscore").innerHTML = table;
+
+    let endDiv = document.getElementById('result');
+    endDiv.style.display = 'block';
+}
+
+function save() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function() {
+        highscore(this.responseText);
+    }
+    xhttp.open("GET", "bubble.php?action=save" +
+        "&points=" + points +
+        "&firstname=" + document.getElementById('firstname').value +
+        "&lastname=" + document.getElementById('lastname').value, true);
+    xhttp.send();
+}
+
 function lost() {
     clearInterval(createInterval);
     clearInterval(moveInterval);
     clearInterval(speedInterval);
     clearInterval(legendInterval);
-    let endDiv = document.getElementById('result');
-    endDiv.style.display = 'block';
+
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function() {
+        highscore(this.responseText);
+    }
+    xhttp.open("GET", "bubble.php?points=" + points, true);
+    xhttp.send();
 }
 
 function speedUp() {
@@ -29,10 +103,13 @@ function speedUp() {
 
 function legendUpdate() {
     time -= 1;
+    points = (hits * 1000) + (speed * 5 * 10) + (10 - time);
     let timeElement = document.getElementById('time');
     timeElement.innerText = parseInt(time) + 's';
     let speedElement = document.getElementById('speed');
     speedElement.innerText = parseInt(speed * 5 + "") + "";
+    let pointsElement = document.getElementById('points');
+    pointsElement.innerText = parseInt(points + "") + "";
 }
 
 function createBubble() {
@@ -66,6 +143,7 @@ function randomColor() {
 }
 
 function removeBubble() {
+    hits++;
     document.body.removeChild(this);
 }
 
